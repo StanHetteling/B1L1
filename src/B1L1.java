5dcd4f4d6ad59e44805ff201a7fc542bd7572a52
StanHetteling <shetteling@gmail.com>
2018-10-17 09:38:07 +0200

    extra tekst

---
+++ src/B1L1.java
@@ -0,0 +1,14 @@
+public class B1L1 {
+
+    public static void main (String[] args)
+    {
+
+        String tekst = "Hallo,";
+        String tekst2 = " Hoe gaat het?";
+        String tekst3 = " Met mij gaat het goed.";
+        System.out.println(tekst + tekst2 + tekst3);
+
+
+
+    }
+}
